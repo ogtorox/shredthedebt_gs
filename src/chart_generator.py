cd4ef3d9a6660f93f83abc2cc9e1832be6384528
Alexis Lohman <Alexis@grandma-potatoe.local>
2025-03-02 01:01:41 -0600

    sf

--- src/chart_generator.py
+++ src/chart_generator.py
@@ -90,7 +90,7 @@ df["Donation"] = df["Donation"].map("${:,.2f}".format)
 
 
 # col1, col2, col3 = st.columns([1, 1, 1])
-col1, col2, col3, col4 = st.columns([1, 0.5, 2])
+col1, col2, col3, col4 = st.columns([1, 0.5, 1, 1])
 with col1:
     st.metric(label="Total Donations", value=f"${total_donations:,.2f}", delta=f"Goal: $15,000") 
 with col3:   
