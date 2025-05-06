73f8dc6364910e936e931be7fe4df9c310d923ef
ogtorox <ailohm27@gmail.com>
2025-05-06 02:33:12 +0000

    cleaning up files

--- src/app.py
+++ src/app.py
@@ -1,1 +1,13 @@
 streamlit run chart_generator.py
+
+# Define page color
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #f4f4f4;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
